6b7a5d50adf524463fe0fa7af97f738bdb4bc560
Kristian Kjems <kjems@playdeadgames.com>
2015-05-14 13:02:45 +0200

    Moved the string specific part of the code into the mail .cs file to mimic the F# implementations

--- source/ComposedSet.cs
+++ source/ComposedSet.cs
@@ -186,3 +186,27 @@ namespace ComposedSet.CSharp
         }
     }
 }
+
+namespace ComposedSet.CSharp
+{
+    using System.Text.RegularExpressions;
+    public class StringComposedSetDatabase : BaseComposedSetDatabase<string>
+    {
+        const string regexSplitter = @"("")|(\])|(\[)|(\t)|(:)|(')|(;)|(-)|(\?)|(!)|(\r)|(\n)|(,)|(\ )|(\.)|(\/)|(\@)|(_)|(\f)";
+        static Regex regex = new Regex(regexSplitter, RegexOptions.Compiled);
+        public override string[] Split(string composed)
+        {
+            return regex.Split(composed).Where(s => !string.IsNullOrEmpty(s)).ToArray();
+        }
+
+        public override string Compose(List<int> indices)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0, length = indices.Count; i < length; ++i)
+            {
+                sb.Append(Parts[indices[i]]);
+            }
+            return sb.ToString();
+        }
+    }
+}
